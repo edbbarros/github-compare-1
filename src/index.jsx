626c8e23dc40cbc7a887bdabdaaed1f64cfb8e84
Pedro Machado <phfm@cin.ufpe.br>
2022-04-03 17:35:13 -0300

    feat: incluindo context para os icons da clayui

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ClayIconSpriteContext } from '@clayui/icon';
 import App from './App';
+import spritemap from './assets/icons.svg';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <ClayIconSpriteContext.Provider value={spritemap}>
+    <App />
+  </ClayIconSpriteContext.Provider>,
+);
