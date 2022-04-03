d07d387d347c5fc5009cc86bc1bd9698e01d2bab
Pedro Machado <phfm@cin.ufpe.br>
2022-04-03 17:38:46 -0300

    feat: componente referente ao conteúdo

---
+++ src/layout/Content/index.jsx
@@ -0,0 +1,17 @@
+function Content() {
+  return (
+    <div
+      style={{
+        backgroundColor: '#E5E5E5',
+        minHeight: 'calc(100vh - 4rem)',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      Content
+    </div>
+  );
+}
+
+export default Content;
