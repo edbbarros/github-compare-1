ac87840a1cea14a563bd65e40ac5c971db8a944c
Pedro Machado <phfm@cin.ufpe.br>
2022-04-04 09:32:43 -0300

    feat: listagem dinâmica dos repositórios

--- src/layout/Content/index.jsx
+++ src/layout/Content/index.jsx
@@ -1,13 +1,16 @@
+import { useState, useContext } from 'react';
 import EmptyState from '../../components/EmptyState';
 import Card from '../../components/Repository/Card';
 import List from '../../components/Repository/List';
 import styles from './Content.module.css';
+import { RepositoryContext } from '../../contexts/RepositoryContext';
 
 function Content() {
-  const showCards = true;
-  const dataLength = [{}];
+  const { repositories } = useContext(RepositoryContext);
 
-  if (!dataLength.length) {
+  const showCards = false;
+
+  if (!repositories.length) {
     return (
       <div className={styles.emptyStateContainer}>
         <EmptyState />
@@ -19,20 +22,39 @@ function Content() {
     <div className={styles.contentContainer}>
       {showCards ? (
         <div className={styles.cardContainer}>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {repositories.map(el => (
+            <Card
+              id={el.id}
+              title={el.title}
+              avatar={el.avatar}
+              stars={el.stars}
+              forks={el.forks}
+              openIssues={el.openIssues}
+              age={el.age}
+              lastCommit={el.lastCommit}
+              license={el.license}
+              language={el.language}
+              isFavorite={el.isFavorite}
+            />
+          ))}
         </div>
       ) : (
         <div className={styles.listContainer}>
-          <List />
-          <List />
-          <List />
-          <List />
+          {repositories.map(el => (
+            <List
+              id={el.id}
+              title={el.title}
+              avatar={el.avatar}
+              stars={el.stars}
+              forks={el.forks}
+              openIssues={el.openIssues}
+              age={el.age}
+              lastCommit={el.lastCommit}
+              license={el.license}
+              language={el.language}
+              isFavorite={el.isFavorite}
+            />
+          ))}
         </div>
       )}
     </div>
