ff8a804e1bac4eb2d85a996f7df1d029d0c19358
Pedro Machado <phfm@cin.ufpe.br>
2022-04-04 21:49:06 -0300

    refactor: ajustes de estilo para filter

--- src/components/Filter/index.jsx
+++ src/components/Filter/index.jsx
@@ -3,51 +3,44 @@ import ClayIcon from '@clayui/icon';
 import ClayButton from '@clayui/button';
 import ClayDropDown from '@clayui/drop-down';
 import { RepositoryContext } from '../../contexts/RepositoryContext';
+import styles from './Filter.module.css';
 
 function Filter() {
   const { sortRepositories } = useContext(RepositoryContext);
 
   const [active, setActive] = useState(false);
 
+  const labels = [
+    { label: 'Stars', value: 'stars' },
+    { label: 'Forks', value: 'forks' },
+    { label: 'Open Issues', value: 'openIssues' },
+    { label: 'Age', value: 'age' },
+    { label: 'Last commit', value: 'lastCommit' },
+  ];
+
   return (
     <ClayDropDown
+      className={styles.dropdown}
       trigger={
         <ClayButton className="nav-link" displayType="unstyled">
           <span className="navbar-breakpoint-down-d-none">
             <span className="navbar-text-truncate">Filter and order</span>
-
             <ClayIcon
               className="inline-item inline-item-after"
               symbol="caret-bottom"
             />
           </span>
-          <span className="navbar-breakpoint-d-none">
-            <ClayIcon symbol="filter" />
-          </span>
         </ClayButton>
       }
       active={active}
       onActiveChange={setActive}
-      menuElementAttrs={{
-        className: 'my-custom-dropdown-menu',
-        containerProps: {
-          className: 'dropdown-menu-react-portal-div',
-          id: 'dropdownMenuReactPortalDiv',
-        },
-      }}
     >
       <ClayDropDown.ItemList>
         <ClayDropDown.Group header="Order by">
-          {[
-            { label: 'Stars', value: 'stars' },
-            { label: 'Forks', value: 'forks' },
-            { label: 'Open Issues', value: 'openIssues' },
-            { label: 'Age', value: 'age' },
-            { label: 'Last commit', value: 'lastCommit' },
-          ].map((item, i) => (
+          {labels.map((item, i) => (
             <ClayDropDown.Item
-              onClick={() => sortRepositories(item.value)}
               key={i}
+              onClick={() => sortRepositories(item.value)}
             >
               {item.label}
             </ClayDropDown.Item>
