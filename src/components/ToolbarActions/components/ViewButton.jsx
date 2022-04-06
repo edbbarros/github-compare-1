da7c14c3606635c07b71474c5a96cd04b2b1e9fe
Pedro Machado <phfm@cin.ufpe.br>
2022-04-06 17:39:41 -0300

    feat: dividindo as seções da toolbar em componentes

---
+++ src/components/ToolbarActions/components/ViewButton.jsx
@@ -0,0 +1,53 @@
+import { useContext } from 'react';
+import ClayManagementToolbar from '@clayui/management-toolbar';
+import ClayIcon from '@clayui/icon';
+import ClayButton from '@clayui/button';
+import { ClayDropDownWithItems } from '@clayui/drop-down';
+import { RepositoryContext } from '../../../contexts/RepositoryContext';
+import { ThemeContext } from '../../../contexts/ThemeContext';
+import styles from '../ToolbarActions.module.css';
+
+function ToolbarActions() {
+  const { isDarkTheme } = useContext(ThemeContext);
+
+  const { repositoriesView, setRepositoriesView } =
+    useContext(RepositoryContext);
+
+  const viewTypes = [
+    {
+      active: repositoriesView === 'cards',
+      label: 'Cards',
+      onClick: () => setRepositoriesView('cards'),
+      symbolLeft: 'cards2',
+    },
+    {
+      active: repositoriesView === 'list',
+      label: 'List',
+      onClick: () => setRepositoriesView('list'),
+      symbolLeft: 'cards-full',
+    },
+  ];
+
+  const viewTypeActive = viewTypes.find(type => type.active);
+
+  return (
+    <ClayManagementToolbar.Item className={styles.displayType}>
+      <ClayDropDownWithItems
+        items={viewTypes}
+        trigger={
+          <ClayButton
+            className="nav-link-monospaced nav-link"
+            displayType="unstyled"
+          >
+            <ClayIcon
+              color={isDarkTheme ? '#FFF' : null}
+              symbol={viewTypeActive ? viewTypeActive.symbolLeft : ''}
+            />
+          </ClayButton>
+        }
+      />
+    </ClayManagementToolbar.Item>
+  );
+}
+
+export default ToolbarActions;
