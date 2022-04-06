c6e4602a01e97208e95c1733d2e6660b6d30c7b6
Pedro Machado <phfm@cin.ufpe.br>
2022-04-06 17:12:47 -0300

    feat: dividindo as seções de um repositório em header e body

--- src/components/Repository/index.jsx
+++ src/components/Repository/index.jsx
@@ -1,12 +1,9 @@
 import { useContext } from 'react';
-import ClayButton from '@clayui/button';
 import ClayCard from '@clayui/card';
 import ClayLabel from '@clayui/label';
-import ClayIcon from '@clayui/icon';
-import DeletionModal from '../DeletionModal';
-import { RepositoryContext } from '../../contexts/RepositoryContext';
 import { ThemeContext } from '../../contexts/ThemeContext';
-import { formatTime } from '../../utils';
+import Header from './components/Header';
+import Body from './components/Body';
 import styles from './Repository.module.css';
 
 function Repository(props) {
@@ -27,8 +24,6 @@ function Repository(props) {
 
   const { isDarkTheme } = useContext(ThemeContext);
 
-  const { addRepositoryToFavorites, removeRepositoryFromFavorites } =
-    useContext(RepositoryContext);
   return (
     <ClayCard
       key={id}
@@ -42,77 +37,22 @@ function Repository(props) {
           : null
       }
     >
-      {/* repository header */}
-      <div
-        className={
-          repositoriesView === 'cards' ? styles.cardHeader : styles.listHeader
-        }
-      >
-        <div
-          className={
-            repositoriesView === 'cards'
-              ? styles.cardRepositoryInfo
-              : styles.listRepositoryInfo
-          }
-        >
-          <img src={avatar} alt="Avatar" />
-          {title}
-        </div>
-        <div
-          className={
-            repositoriesView === 'cards'
-              ? styles.cardRepositoryActions
-              : styles.listRepositoryActions
-          }
-        >
-          <ClayButton
-            displayType="secondary"
-            borderless
-            onClick={() => {
-              if (isFavorite) {
-                removeRepositoryFromFavorites(id);
-              } else {
-                addRepositoryToFavorites(id);
-              }
-            }}
-          >
-            {isFavorite ? (
-              <ClayIcon symbol="star" />
-            ) : (
-              <ClayIcon symbol="star-o" />
-            )}
-          </ClayButton>
-          <DeletionModal title={title} id={id} />
-        </div>
-      </div>
-
-      {/* repository body */}
-      <div
-        className={
-          repositoriesView === 'cards' ? styles.cardBody : styles.listBody
-        }
-      >
-        <p>
-          Stars <span>{stars}</span>
-        </p>
-        <p>
-          Forks <span>{forks}</span>
-        </p>
-        <p>
-          Open Issues <span>{openIssues}</span>
-        </p>
-        <p>
-          Age <span>{formatTime(age)}</span>
-        </p>
-        <p>
-          Last commit <span>{formatTime(lastCommit)}</span>
-        </p>
-        <p>
-          License <span>{license?.name || 'N/A'}</span>
-        </p>
-      </div>
-
-      {/* language label) */}
+      <Header
+        repositoriesView={repositoriesView}
+        id={id}
+        title={title}
+        avatar={avatar}
+        isFavorite={isFavorite}
+      />
+      <Body
+        repositoriesView={repositoriesView}
+        stars={stars}
+        forks={forks}
+        openIssues={openIssues}
+        age={age}
+        lastCommit={lastCommit}
+        licens={license}
+      />
       <ClayLabel
         key={id}
         className={
