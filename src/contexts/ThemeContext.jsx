8eb13db5fb10f9b2635eea04643a7d5aeaeb7fe3
Pedro Machado <phfm@cin.ufpe.br>
2022-04-06 10:47:06 -0300

    feat: context para o tema

---
+++ src/contexts/ThemeContext.jsx
@@ -0,0 +1,19 @@
+import { createContext, useMemo, useState } from 'react';
+import api from '../services/api';
+
+const initialValue = {};
+
+export const ThemeContext = createContext(initialValue);
+
+export function ThemeContextProvider({ children }) {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const value = useMemo(
+    () => ({ isDarkTheme, setIsDarkTheme }),
+    [isDarkTheme, setIsDarkTheme],
+  );
+
+  return (
+    <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
+  );
+}
--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { ClayIconSpriteContext } from '@clayui/icon';
 import App from './App';
 import { RepositoryContextProvider } from './contexts/RepositoryContext';
+import { ThemeContextProvider } from './contexts/ThemeContext';
 import spritemap from './assets/icons.svg';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ClayIconSpriteContext.Provider value={spritemap}>
-    <RepositoryContextProvider>
-      <App />
-    </RepositoryContextProvider>
+    <ThemeContextProvider>
+      <RepositoryContextProvider>
+        <App />
+      </RepositoryContextProvider>
+    </ThemeContextProvider>
   </ClayIconSpriteContext.Provider>,
 );
