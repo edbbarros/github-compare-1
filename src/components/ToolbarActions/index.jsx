d0e5c72de95aed18ea235f841a4ca5fe75c67813
Pedro Machado <phfm@cin.ufpe.br>
2022-04-06 20:19:54 -0300

    style: ajuste na ordenação das declarações

--- src/components/ToolbarActions/index.jsx
+++ src/components/ToolbarActions/index.jsx
@@ -1,9 +1,9 @@
 import ClayManagementToolbar from '@clayui/management-toolbar';
-import NewRepositoryButtom from './components/NewRepositoryButtom';
 import MobileSearchButton from './components/MobileSearchButton';
 import FavoritesButton from './components/FavoritesButton';
 import ThemeButton from './components/ThemeButton';
 import ViewButton from './components/ViewButton';
+import NewRepositoryButtom from './components/NewRepositoryButtom';
 
 function ToolbarActions() {
   return (
